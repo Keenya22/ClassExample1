7902dde5c6602cbce4b95321d64bfd6ae3daabc9
Keenya22 <keenyagilchrist@hotmail.com>
2019-01-23 12:24:29 -0600

    Changed text to Hola Mundo!

---
+++ app/src/main/java/edu/apsu/csci/classexample/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.apsu.csci.classexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //make text say hello
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView) findViewById(R.id.textView)).setText("Hola Mundo!");
+            }
+        });
+    }
+}
